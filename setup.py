03c721074c18ff6fbfa6c440cd357e568ee1800a
Julien Maupetit <julien@maupetit.net>
2018-03-22 10:49:58 +0100

    feat(package) add setup script and configuration

--- setup.py
+++ setup.py
@@ -1,24 +1,5 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-setup(
-    name='fun-cmd',
-    version='0.1',
-    description="Command runner for OpenFUN",
-    long_description="Commands for running OpenFUN LMS or CMS systems",
-    classifiers=[],
-    keywords='',
-    author=u"France Université Numérique",
-    author_email='regis@openfun.fr',
-    url='https://github.com/openfun/fun-cmd.git',
-    license='MIT',
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'fun = funcmd.cmd:fun',
-        ]
-    },
-)
-
+setup()
